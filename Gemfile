e7df67a4fb92d1cbda834648c821dc34d0a0ceb3
Junichi Ito <jit@sonicgarden.jp>
2022-01-01 17:49:50 +0900

    Add 02-setup

--- Gemfile
+++ Gemfile
@@ -60,7 +60,7 @@ group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ], require: false
 
-  gem 'rspec-rails'
+  gem 'rspec-rails' # add in chapter 2
   gem 'factory_bot_rails'
 end
 
--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,14 @@ module Projects
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+      g.factory_bot false
+    end
   end
 end
