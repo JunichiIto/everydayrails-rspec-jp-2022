058db2e82fdd713bcc242589cf8957806ad8f33b
sandonemaki <kamuramaki@gmail.com>
2023-11-20 18:02:20 +0900

    不要なテストファイルを作らない設定を追加

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,11 @@ module Projects
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      fixtures: false,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false
+    end
   end
 end
