8b6c1f63ff4b816c5532c8f6cf3a31deda4078d9
sandonemaki <kamuramaki@gmail.com>
2023-11-20 23:11:47 +0900

    projectモデルスペック作成

---
+++ spec/models/project_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe Project, type: :model do
+  it "ユーザー単位では重複したプロジェクト名を許可しないこと" do
+    user = User.create(
+      first_name: "Joe",
+      last_name: "Tester",
+      email: "joetester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze",
+    )
+
+    user.projects.create(
+      name: "Test Project",
+    )
+
+    new_project = user.projects.build(
+      name: "Test Project",
+    )
+
+    new_project.valid?
+    expect(new_project.errors[:name]).to include("has already been taken")
+  end
+
+  it "2人のユーザーが同じ名前を使うことは許可すること" do
+    user = User.create(
+      first_name: "Joe",
+      last_name: "Tester",
+      email: "joetester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze",
+    )
+
+    user.projects.create(
+      name: "Test Project",
+    )
+
+    other_user = User.create(
+      first_name: "Jane",
+      last_name: "Tester",
+      email: "janetester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze",
+    )
+
+    other_project = other_user.projects.build(
+      name: "Test Project",
+    )
+
+    expect(other_project).to be_valid
+  end
+end
+
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "姓、名、メール、パスワードがあれば有効な状態であること"
-  it "名がなければ無効な状態であること"
-  it "姓がなければ無効な状態であること"
-  it "メールアドレスがなければ無効な状態であること"
-  it "重複したメールアドレスなら無効な状態であること"
-  it "ユーザーのフルネームを⽂字列として返すこと"
+  it "姓、名、メール、パスワードがあれば有効な状態であること" do
+    user = User.new(
+      first_name: "Aaron",
+      last_name: "Sumner",
+      email: "tester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze",
+    )
+    expect(user).to be_valid
+  end
+
+  it "名がなければ無効な状態であること" do
+    user = User.new(first_name: nil)
+    user.valid?
+    expect(user.errors[:first_name]).to include("can't be blank")
+  end
+
+  it "姓がなければ無効な状態であること" do
+    user = User.new(last_name: nil)
+    user.valid?
+    expect(user.errors[:last_name]).to include("can't be blank")
+  end
+
+  it "重複したメールアドレスなら無効な状態であること" do
+    User.create(
+      first_name: "Joe",
+      last_name: "Tester",
+      email: "tester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze",
+    )
+    user = User.new(
+      first_name: "Jane",
+      last_name: "Tester",
+      email: "tester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze",
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
+
+  it "ユーザーのフルネームを⽂字列として返すこと" do
+    user = User.new(
+      first_name: "John",
+      last_name: "Doe",
+      email: "johndoe@example.com",
+    )
+    expect(user.name).to eq "John Doe"
+  end
 end
